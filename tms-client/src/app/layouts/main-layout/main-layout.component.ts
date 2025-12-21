a9e60e4c5687814d15f94fb2258462e48ce66eeb
Sara King <sara@sara-king.com>
2025-12-21 00:50:32 -0600

    removed aside-nav

--- tms-client/src/app/layouts/main-layout/main-layout.component.ts
+++ tms-client/src/app/layouts/main-layout/main-layout.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AsideNavbarComponent } from '../../aside-navbar/aside-navbar.component';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [RouterOutlet, AsideNavbarComponent],
+  imports: [RouterOutlet],
   template: `
     <div class="layout">
-      <app-aside-navbar class="layout_aside"></app-aside-navbar>
       <section class="layout_content">
         <router-outlet></router-outlet>
       </section>
@@ -22,6 +20,6 @@ import { AsideNavbarComponent } from '../../aside-navbar/aside-navbar.component'
       .layout { flex-direction: column; }
       .layout_aside { position: static; flex: 0 0 auto; }
     }
-  `
+  `,
 })
 export class MainLayoutComponent {}
